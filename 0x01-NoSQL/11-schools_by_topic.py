efcbc989fe63447501a4b4f3c1a0673b216c3dd9
ofhatwa <tofhatwa@gmail.com>
2024-10-22 23:22:25 +0200

    Adding a python file that returns lsits of a school based on the topics

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This module return lists of school by topic"""
+
+def schools_by_topic(mongo_collection, topic):
+    """ Return a list of schools having a specific topic
+
+    Args:
+        mongo_collection: The pymongo collection object
+        topic (str): The topic to search for
+
+    Returns:
+        A list of schools that have the specified topic
+    """
+    results = mongo_collection.find({"topics": topic})
+    return list(results)
